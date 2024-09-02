3416be4db699eb20ceb7c17826ba2b30d8992c5b
Pamela Nakano <pamela.nakano@hotmail.com>
2024-09-02 19:20:51 -0300

    Adicionado Toolbar, implementado componente de erro do input, melhorias no form de cadastro de propostas

--- src/app/modules/proposta-financeira/components/crud/crud.component.ts
+++ src/app/modules/proposta-financeira/components/crud/crud.component.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { PropostaFinanceiraService } from '../../services/proposta-financeira.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Location } from '@angular/common';
 import {
-  FormControl,
+  FormBuilder,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
@@ -14,6 +15,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+import { NgxMaskDirective } from 'ngx-mask';
+import { InputErrorComponent } from '../../../../shared/components/input-error/input-error.component';
 
 @Component({
   selector: 'app-crud',
@@ -28,36 +31,38 @@ import { MatCardModule } from '@angular/material/card';
     ReactiveFormsModule,
     MatSliderModule,
     MatCardModule,
+    NgxMaskDirective,
+    InputErrorComponent,
   ],
   providers: [PropostaFinanceiraService],
   templateUrl: './crud.component.html',
   styleUrl: './crud.component.scss',
 })
 export class CrudComponent {
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  crudForm: FormGroup = this.formBuilder.group({
+    nome: [null, Validators.required],
+    sobrenome: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]],
+    telefone: [null, Validators.required],
+    cpf: [null, Validators.required],
+    renda: [null, Validators.required],
+    valorSolicitado: [null, Validators.required],
+    prazoPagamento: [null, null],
+  });
 
   constructor(
     private propostaFinanceiraService: PropostaFinanceiraService,
-    private router: Router,
-    private location: Location
+    private location: Location,
+    private formBuilder: FormBuilder
   ) {}
 
   postProposta(): void {
-    let proposta = {
-      nome: 'teste',
-      sobrenome: 'testes',
-      telefone: '(11) 11111-1111',
-      cpf: '123.456.789-01',
-      email: 'pamela.nakano@hotmail.com',
-      renda: 10000.0,
-      valorSolicitado: 5000.0,
-      prazoPagamento: 6,
-    };
+    this.crudForm.markAllAsTouched();
+    if (!this.crudForm.valid) {
+      return;
+    }
     this.propostaFinanceiraService
-      .postProposta(proposta)
+      .postProposta(this.crudForm.value)
       .subscribe((proposta) => {
         console.log(proposta);
         this.goBack();
